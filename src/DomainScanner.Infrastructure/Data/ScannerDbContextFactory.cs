9a505afdd310eff68ea43762f46d01fd18890122
gesefaki <gesefaki1@gmail.com>
2025-11-20 00:17:44 +0300

    добавлена фабрика db context

---
+++ src/DomainScanner.Infrastructure/Data/ScannerDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DomainScanner.Infrastructure.Data;
+
+public class ScannerDbContextFactory : IDesignTimeDbContextFactory<ScannerDbContext>
+{
+    public ScannerDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<ScannerDbContext>();
+        
+        builder.UseNpgsql("Host=localhost;Database=domainscanner;Username=postgres;Password=postgres");
+        
+        return new ScannerDbContext(builder.Options);
+    }
+}
