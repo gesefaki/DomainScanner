47c4864c0922da96fc44bd557be232ba79ce31cb
gesefaki <gesefaki1@gmail.com>
2025-11-20 06:04:36 +0300

    DTO added

---
+++ src/DomainScanner.Api/DTOs/CreateDomainDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomainScanner.Api.DTOs;
+
+public class CreateDomainDto
+{
+    public int Id { get; set; }
+    
+    [Required, MaxLength(256)]
+    public required string Name { get; init; }
+}
---
+++ src/DomainScanner.Api/DTOs/DomainDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomainScanner.Api.DTOs;
+
+public class DomainDto
+{
+    public int Id { get; set; }
+    [Required, MaxLength(256)]
+    public required string Name { get; set; }
+    
+    public bool? IsAvailable { get; set; }
+}
